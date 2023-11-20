626697e44fc5899bbb517cce7ad070e72ce4f0e5
cclarke74 <clairc2@gmail.com>
2023-11-20 14:47:46 -0800

    first commit

---
+++ src/main/java/com/simplilearn/spring/SimplilearnSpringJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplilearnSpringJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplilearnSpringJdbcApplication.class, args);
+	}
+
+}
