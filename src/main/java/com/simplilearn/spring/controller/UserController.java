bebbf6e49647a30420b9d38f752bad4610e3b77c
cclarke74 <clairc2@gmail.com>
2023-11-20 16:24:15 -0800

    Add Logger

--- src/main/java/com/simplilearn/spring/controller/UserController.java
+++ src/main/java/com/simplilearn/spring/controller/UserController.java
@@ -1,14 +1,40 @@
 package com.simplilearn.spring.controller;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.simplilearn.spring.bean.User;
+import com.simplilearn.spring.service.UserService;
 
 @Controller
 public class UserController {
 
+	final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	UserService userService;
+	
 	@GetMapping("/")
 	String showIndex() {
 		
+		logger.debug("Showing Index...");
+		
 		return "index";
 	}
+	@GetMapping("/list")
+	ModelAndView list() {
+		
+		logger.debug("Listing Users...");
+
+		List<User> users = this.userService.list();
+		
+		return new ModelAndView("list", "users", users);
+	}
 }
+
--- src/main/java/com/simplilearn/spring/dao/mapper/UserMapper.java
+++ src/main/java/com/simplilearn/spring/dao/mapper/UserMapper.java
@@ -11,7 +11,17 @@ public class UserMapper implements RowMapper<User> {
 
 	@Override
 	public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-		return null;
-	}
+		User user = new User();
+
+        user.setIdUser(rs.getInt("ID_USER"));
+        user.setUsername(rs.getString("USERNAME"));
+        user.setPassword(rs.getString("PASSWORD"));
+        user.setFirstName(rs.getString("FIRST_NAME"));
+        user.setLastName(rs.getString("LAST_NAME"));
+        user.setBirth(rs.getDate("BIRTH"));
+        user.setStatus(rs.getString("STATUS"));
+
+        return user;	
+        }
 
 }
