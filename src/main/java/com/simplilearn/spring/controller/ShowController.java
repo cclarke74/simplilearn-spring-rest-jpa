52a131c92ed46ca1d562d59d7787ec9e221a4098
cclarke74 <clairc2@gmail.com>
2023-12-02 12:19:37 -0800

    first commit

--- src/main/java/com/simplilearn/spring/controller/ShowController.java
+++ src/main/java/com/simplilearn/spring/controller/ShowController.java
@@ -5,47 +5,59 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.simplilearn.spring.jpa.User;
 import com.simplilearn.spring.service.UserService;
 
-@RestController
-public class UserController {
+@Controller
+public class ShowController {
 
 	final Logger logger = LoggerFactory.getLogger(this.getClass());
 	
-	@Autowired
-	UserService userService;
+
 	
-	@GetMapping("/users")
-	List<User> list() {
+	@GetMapping("/")
+	String showIndex() {
 		
-		logger.debug("Listing Users...");
+		logger.debug("Showing Index...");
 		
-		return this.userService.list();
+		return "index";
 	}
 	
+	@GetMapping("/list")
+	String list() {
+		
+		logger.debug("Showing Users...");
+		
+		return "list";
+	}
+	
+	@GetMapping("/create")
+	String showCreate(User user) {
+		
+		logger.debug("Showing Create...");
+		
+		return "create";
+	}
+	
+	/*
 	@PostMapping("/create")
 	String createUser(User user, BindingResult result) {
 		
 		logger.debug("Creating User... {}", user);
-		
-		this.validateUsername(user, result);
-		
+				
 		if(result.hasErrors()) {
 			return "create";
 		}
-		
-		this.userService.createUser(user);
-		
+				
 		return "redirect:/list";
 	}
 	
@@ -53,24 +65,18 @@ public class UserController {
 	ModelAndView showUpdate(@PathVariable int id) {
 		
 		logger.debug("Showing Update.. UserID: {}", id);
-		
-		User user = this.userService.findUser(id);
-		
+				
 		return new ModelAndView("update", "user", user);
 	}
 	@PostMapping("/update")
 	String updateUser(User user, BindingResult result) {
 		
 		logger.debug("Updating User... {}", user);
-		
-		this.validateUsername(user, result);
-		
+				
 		if(result.hasErrors()) {
 			return "update";
 		}
-		
-		this.userService.updateUser(user);
-		
+				
 		return "redirect:/list";
 	}
 	
@@ -78,9 +84,7 @@ public class UserController {
 	String deleteUser(@PathVariable int id) {
 		
 		logger.debug("Deleting User.. UserID: {}", id);
-		
-		this.userService.deleteUser(id);
-		
+				
 		return "redirect:/list";
 	}
 	
@@ -96,20 +100,10 @@ public class UserController {
 	ModelAndView searchUser(@RequestParam int id) {
 		
 		logger.debug("Searching User.. UserID: {}", id);
-		
-		User user = this.userService.findUser(id);
-		
+				
 		return new ModelAndView("table", "user", user);
-	}
+	}*/
 	
-	void validateUsername(User user, BindingResult result) {
-		
-		if ( this.userService.findUser(user) != null){
-		//if ( Optional.ofNullable(this.userService.findUser(user)).isPresent() ){
-			
-			result.addError(new FieldError("user", "username", user.getUsername(), 
-											false, null, null, "* Username already taken."));
-		}
-	}
 }
+
 
