0e2f0ca9b31899287478ee1c689bedb522d1ab8e
cclarke74 <clairc2@gmail.com>
2023-12-03 16:02:44 -0800

    Add Search

--- src/main/java/com/simplilearn/spring/service/UserService.java
+++ src/main/java/com/simplilearn/spring/service/UserService.java
@@ -11,55 +11,60 @@ import com.simplilearn.spring.repository.UserRepository;
 @Service
 public class UserService {
 
-	@Autowired
-	UserRepository userRepository;
-	
-	public List<User> list(){
-		
-		/*
-		 * Add some Business Logic to process the list.
-		 */
-		
-		return this.userRepository.findAll();
-	}
-	
-	public void createUser(User user) {
-		this.validateUser(user);
-		
-		user.setStatus("A");
-		this.userRepository.save(user);
-	}
-	
-	public User findUser(User user) {
-		return this.userRepository.findByIdUserNotAndUsernameIgnoreCase(user.getIdUser(), user.getUsername());
-	}
-	public User findUser(int idUser) {
-		return this.userRepository.findById(idUser).orElseGet(null);
-	}
-	
-	public void updateUser(User user) {
-		this.validateUser(user);
-		
-		this.userRepository.findById(user.getIdUser())
-								.ifPresent(u -> {
-									u.setFirstName(user.getFirstName());
-									u.setLastName(user.getLastName());
-									u.setUsername(user.getUsername());
-									u.setBirth(user.getBirth());
-									
-									this.userRepository.save(u);
-								});
-				
-	}
-	
-	public void deleteUser(int idUser) {
-		this.userRepository.deleteById(idUser);
-	}
-	
-	private void validateUser(User user) {
-		if (user.getFirstName().isEmpty() || user.getLastName().isEmpty()  || user.getUsername().isEmpty()) {
-			throw new RuntimeException("Invalid User Data: " +user);
-			
-		}
-	}
+    @Autowired
+    UserRepository userRepository;
+
+    public List<User> list() {
+
+        /*
+         * Add some Business Logic to process the list.
+         */
+
+        return this.userRepository.findAll();
+    }
+
+    public void createUser(User user) {
+
+        this.validateUser(user);
+
+        user.setStatus("A");
+        this.userRepository.save(user);
+    }
+
+    public User findUser(User user) {
+        return this.userRepository.findByIdUserNotAndUsernameIgnoreCase(user.getIdUser(), user.getUsername());
+    }
+
+
+    public User findUser(int idUser) {
+        return this.userRepository.findById(idUser).orElse(null);
+    }
+
+    public void updateUser(User user) {
+
+        this.validateUser(user);
+
+        this.userRepository.findById(user.getIdUser())
+                              .ifPresent(u -> {
+                                  u.setFirstName(user.getFirstName());
+                                  u.setLastName(user.getLastName());
+                                  u.setUsername(user.getUsername());
+                                  u.setBirth(user.getBirth());
+
+                                  this.userRepository.save(u);
+                              });
+    }
+
+    public void deleteUser(int idUser) {
+        this.userRepository.deleteById(idUser);
+    }
+
+    private void validateUser(User user) {
+        if (user.getFirstName().isEmpty() ||
+            user.getLastName().isEmpty() ||
+            user.getUsername().isEmpty()) {
+            throw new RuntimeException("Invalid User Data: " + user);
+        }
+
+    }
 }
